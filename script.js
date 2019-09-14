55af49ebea1f60730b845de9833d0d186291561c
Tero Viitala <vite19ax@student.ju.se>
2019-09-14 10:16:52 +0200

    Modified

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
+// Skapar en array:
+let sVsM = ['sun', 'moon'];
 
+// Ger åtkomst till element:
 let sun = document.getElementById('sun-b');
 
-
+// Gör om ordningen på arrayn när klickar på bilden:
 sun.addEventListener('click', function() {
-    sun.setAttribute('src', 'moon.jpg');
     
-    console.log(sun);
+    sVsM.shift();
+    sVsM.push('sun');
+    sVsM.push('moon');
+
+    sun.setAttribute('src', '' + sVsM[0]+'.jpg');
 });
